0596f1f813312ce094415844bd6f312c8835ecdb
Ben <tttw216@gmail.com>
2024-06-19 00:13:44 +0800

    [Feat] UiState & ViewModel

---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlin.properties.Delegates
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState()) // StateFlow 是可觀察且不可變動的 Flow
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        TODO("Not yet implemented")
+    }
+}
